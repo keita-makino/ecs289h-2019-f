33a221b5dbf78f1a02aee2ab7bdee027a3c1827c
keita-makino <kemakino@ucdavis.edu>
2019-11-25 12:35:40 -0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Scene from './components/Scene';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Scene />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Scene.tsx
@@ -0,0 +1,56 @@
+import * as BABYLON from 'babylonjs';
+import * as GUI from 'babylonjs-gui';
+import React, { useEffect, useRef } from 'react';
+import data from '../data/lesmis-3d.json';
+
+type Props = {};
+
+const onSceneLoaded = (
+  engine: BABYLON.Engine,
+  scene: BABYLON.Scene,
+  canvas: HTMLCanvasElement
+) => {
+  const camera = new BABYLON.ArcRotateCamera(
+    'camera',
+    0,
+    0,
+    10,
+    new BABYLON.Vector3(0, 0, 0),
+    scene
+  );
+  camera.wheelPrecision = 50;
+  camera.setTarget(BABYLON.Vector3.Zero());
+  camera.attachControl(canvas);
+
+  const light = new BABYLON.HemisphericLight(
+    'light',
+    new BABYLON.Vector3(1, 1, 1),
+    scene
+  );
+  light.intensity = 0.6;
+
+  const sphere = BABYLON.Mesh.CreateSphere('sphere', 100.0, 10.0, scene);
+  sphere.position = new BABYLON.Vector3(0, 0, 0);
+  sphere.rotation.x = Math.PI;
+
+  engine.runRenderLoop(() => {
+    if (scene) {
+      scene.render();
+    }
+  });
+};
+
+const Scene: React.FC<Props> = () => {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = ref.current as HTMLCanvasElement;
+    const engine = new BABYLON.Engine(canvas, true);
+    const scene = new BABYLON.Scene(engine);
+    onSceneLoaded(engine, scene, canvas);
+  });
+
+  return <canvas width="1280px" height="720px" ref={ref} />;
+};
+
+export default Scene;
